1f628d28af59c669cd98e6151f19c831ba0de352
Zee1234 <zeeatgit@gmail.com>
2017-09-14 20:51:47 -0500

    231, 614, 612

--- os/lib/app.lua
+++ os/lib/app.lua
@@ -8,7 +8,7 @@ function app.newTask(dir, tag)
   while true do
     pid = ('%d'):format(love.math.random(1000, 9999))
     good = true
-    for k,v in ipairs(apps) do
+    for _,v in ipairs(apps) do
       if v.pid == pid then good = false end
     end
     if good then break end
@@ -59,23 +59,23 @@ function app.newEnvironment(dir, pen)
     tostring = tostring,
     type = type,
     unpack = unpack,
-    --[[coroutine = { create = coroutine.create, resume = coroutine.resume, 
-        running = coroutine.running, status = coroutine.status, 
+    --[[coroutine = { create = coroutine.create, resume = coroutine.resume,
+        running = coroutine.running, status = coroutine.status,
         wrap = coroutine.wrap },]]
-    string = { byte = string.byte, char = string.char, find = string.find, 
-        format = string.format, gmatch = string.gmatch, gsub = string.gsub, 
-        len = string.len, lower = string.lower, match = string.match, 
-        rep = string.rep, reverse = string.reverse, sub = string.sub, 
+    string = { byte = string.byte, char = string.char, find = string.find,
+        format = string.format, gmatch = string.gmatch, gsub = string.gsub,
+        len = string.len, lower = string.lower, match = string.match,
+        rep = string.rep, reverse = string.reverse, sub = string.sub,
         upper = string.upper },
-    table = { insert = table.insert, maxn = table.maxn, remove = table.remove, 
+    table = { insert = table.insert, maxn = table.maxn, remove = table.remove,
         sort = table.sort },
-    math = { abs = math.abs, acos = math.acos, asin = math.asin, 
-        atan = math.atan, atan2 = math.atan2, ceil = math.ceil, cos = math.cos, 
-        cosh = math.cosh, deg = math.deg, exp = math.exp, floor = math.floor, 
-        fmod = math.fmod, frexp = math.frexp, huge = math.huge, 
-        ldexp = math.ldexp, log = math.log, log10 = math.log10, max = math.max, 
-        min = math.min, modf = math.modf, pi = math.pi, pow = math.pow, 
-        rad = math.rad, random = math.random, sin = math.sin, sinh = math.sinh, 
+    math = { abs = math.abs, acos = math.acos, asin = math.asin,
+        atan = math.atan, atan2 = math.atan2, ceil = math.ceil, cos = math.cos,
+        cosh = math.cosh, deg = math.deg, exp = math.exp, floor = math.floor,
+        fmod = math.fmod, frexp = math.frexp, huge = math.huge,
+        ldexp = math.ldexp, log = math.log, log10 = math.log10, max = math.max,
+        min = math.min, modf = math.modf, pi = math.pi, pow = math.pow,
+        rad = math.rad, random = math.random, sin = math.sin, sinh = math.sinh,
         sqrt = math.sqrt, tan = math.tan, tanh = math.tanh },
     os = { clock = os.clock, difftime = os.difftime, time = os.time },
     api = { g = { set=api.g.set, get=api.g.get, text=api.g.text, box=api.g.box },
